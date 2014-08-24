91298bf8ba78fdece5cf05411eff376c0977f7bb
Jerry Yu <hweetty@gmail.com>
2014-08-23 21:53:55 -0400

    Use tabs for indentation, added debug/prod configs

--- back/app.js
+++ back/app.js
@@ -1,20 +1,35 @@
 //Require main modules
 var express = require('express'),
 	mongoose = require('mongoose'),
-	fileSystem = require('fs'),
+	fs = require('fs'),
 	app = express();
 
-//Load configuration file
-var mongo = JSON.parse(fileSystem.readFileSync(__dirname + '/mongodb-private.json'));
-mongoose.connect('mongodb://' + mongo.username +':'+ mongo.password + '@' + mongo.url);
+//Load configuration file (check debug config first)
+var config;
+var paths = [
+	__dirname + "/config-debug-private.json",
+	__dirname + "/config-prod-private.json"
+]
 
-console.log('mongodb://' + mongo.username +':'+ mongo.password + '@' + mongo.url);
+for (var i in paths) {
+	if (fs.existsSync(paths[i])) {
+		config = JSON.parse(fs.readFileSync(paths[i]));
+		break;
+	}
+}
+
+// Connect mongodb
+var mongo = config.mongo;
+var url = 'mongodb://' + mongo.username +':'+ mongo.password + '@' + mongo.url;
+mongoose.connect(url);
+
+console.log(url);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 //Setup express
 app.configure(function() {
 	app.use(express.limit('20mb')); // 10mb hard limit, 2mb warning limit in upload.js
-    app.use(express.bodyParser());  // upload files
+   app.use(express.bodyParser());  // upload files
 
 	app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
@@ -29,12 +44,12 @@ var api = "/api/1/";
 var upload = require('./controllers/upload.js');
 
 // Uploads
-app.get ("/f/:path",         upload.get);
-app.post(api+ "upload",           upload.post);
+app.get ("/f/:path",   		upload.get);
+app.post(api+ "upload",		upload.post);
 app.use("/", express.static("../front"));
 
 
 //Start server
-var server = app.listen(2718);
+var server = app.listen(config.port);
 
-console.log('Node/Express server started!');
+console.log('Server started on port ' + config.port);
